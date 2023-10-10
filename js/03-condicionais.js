1e98f926946ba0a6400c58c720429405c2050397
pato2008 <142536982+pato2008@users.noreply.github.com>
2023-10-10 15:15:49 -0300

    pasoca

---
+++ js/03-condicionais.js
@@ -0,0 +1,15 @@
+/* estrutura de controle condicional comandos que parmitem analisar uma ou mais condiçoes,comandos e a partir de resultado desta analise (verdadeiro ou falso), o programa pode sxecutar açoes diferentes.
+
+comandos mais comuns: if,else,else if*/
+
+let usuario="juean";
+let idade= 25;
+
+
+// verificar a idade e derteminar se e maior ou menor de idade
+
+if(idade >= 18){
+console.log($(usuario) `e maior de idade!` );
+ } else{
+    console.log($(usuario) `e menor de idade`);
+ }
