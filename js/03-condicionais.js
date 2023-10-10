e86a0f9a9201ba9672173f0bd7e7a302d7031b0c
pato2008 <142536982+pato2008@users.noreply.github.com>
2023-10-10 15:23:58 -0300

    fds

--- js/03-condicionais.js
+++ js/03-condicionais.js
@@ -2,14 +2,19 @@
 
 comandos mais comuns: if,else,else if*/
 
-let usuario="juean";
-let idade= 25;
+let usuario = "juean";
+let idade = 25;
 
 
 // verificar a idade e derteminar se e maior ou menor de idade
 
-if(idade >= 18){
-console.log($(usuario) `e maior de idade!` );
- } else{
-    console.log($(usuario) `e menor de idade`);
- }
+if (idade >= 18) {
+    console.log($(usuario)`e maior de idade!`);
+} else {
+    console.log($(usuario)`e menor de idade`);
+}
+
+
+
+
+console.log($(usuario)`e $(mensgen) de idade`);
