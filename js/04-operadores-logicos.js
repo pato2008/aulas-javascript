29b2fedb08122fd119d35f28f64e2327ec9420b5
pato2008 <142536982+pato2008@users.noreply.github.com>
2023-10-11 14:04:04 -0300

    aa

---
+++ js/04-operadores-logicos.js
@@ -0,0 +1,8 @@
+// Requisitos da escola para aprovação
+const mediaMinima = 7;
+const limiteDeFaltas = 10;
+
+// Resultados do aluno fictício
+let aluno = "Jean";
+let mediaFinal = 8.5;
+let faltas = 8;
