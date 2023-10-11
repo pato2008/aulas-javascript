fe543364a28234d4246034359997a94b6936dfe0
pato2008 <142536982+pato2008@users.noreply.github.com>
2023-10-11 16:24:28 -0300

    Update 05-loop.js

--- js/05-loop.js
+++ js/05-loop.js
@@ -8,14 +8,18 @@ comandos tradicionasi:
 obs:normalmnete o jogo e comtrolado atraveis de uma varivel contadora.
 */
 
+//exmplo (while)
+// let contador = 1;
+// while(contador <= 5){
+//     console.log(`valor de contador : ${contador}`);
+// contador++;// ++ incremento
+// }
 
-let contador = 1;
-while(contador <= 5){
-    console.log(`valor de contador : ${contador}`);
-contador++;
+// exmplo do/while
+
+let contador =10;
+do{
+    console.log(`contador vale: ${contador}`);
+    contador++
 }
-
-
-
-
-
+while(contador <= 3)
