f4b5ac93cd101b5659e3a2390dcac3cb28f8471c
pato2008 <142536982+pato2008@users.noreply.github.com>
2023-10-11 14:37:40 -0300

    Update 04-operadores-logicos.js

--- js/04-operadores-logicos.js
+++ js/04-operadores-logicos.js
@@ -1,3 +1,6 @@
+"use strict";
+
+
 // Requisitos da escola para aprovação
 const mediaMinima = 7;
 const limiteDeFaltas = 10;
@@ -6,3 +9,28 @@ const limiteDeFaltas = 10;
 let aluno = "Jean";
 let mediaFinal = 8.5;
 let faltas = 8;
+let resultado;
+/*operdor logico e (and) */
+// if (mediaFinal >= mediaMinima && faltas < limiteDeFaltas) {
+//     resultado = "aprovado"
+// } else{
+//     resultado = "reprovado"
+// }
+
+if (mediaFinal >= mediaMinima && faltas < limiteDeFaltas) {
+     resultado = "aprovado"}
+
+else if ( faltas > limiteDeFaltas){
+resultado= "reprovado pro faltas"
+}
+
+else{
+resultado="reprovado"
+
+}
+
+
+
+console.log(` o aluno ${aluno} esta ${resultad}`);
+console.log( ` media do aluno${mediaFinal}`);
+console.log(`faltas do alumo ${faltas}`)
