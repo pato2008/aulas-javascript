bd54840336fdbafe7a04af86da7a2272ca51b51c
pato2008 <142536982+pato2008@users.noreply.github.com>
2023-10-17 15:19:29 -0300

    i walk

--- js/06-funçoes.js
+++ js/06-funçoes.js
@@ -11,3 +11,41 @@ o js ja possui centenas de fumçoes prontas
 ex:
 log(), alert(),prompt(),etc
 */
+
+/* como escrever funçoes de js */
+
+// forma !:funçao anonimas
+
+    const exemplo1 = function(){
+        console.log("funçao anonima");
+    };
+
+
+    //forma 2 :função nomeada/declarada
+    function exemplo2(){
+        console.log("função nomeada")
+    };
+
+    //forma 3 : arrow funtion
+
+    const exemplo3 = () =>{
+        console.log("arrow funtion");
+    };
+
+// chamada de fundação para execução
+
+exemplo1();
+exemplo2();
+exemplo3();
+
+function somar(valor1,valor2){
+ return valor1+valor2;
+}
+
+
+console.log(somar (10,20));
+console.log(somar (15,50));
+console.log(somar (30,47.8));
+
+
+
